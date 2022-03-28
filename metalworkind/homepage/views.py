f9c6109368feaa133971c7b6a3b9a46011b494c7
Nick <info@tocomm.ru>
2022-03-28 22:36:40 +0300

    gosts about

--- metalworkind/homepage/views.py
+++ metalworkind/homepage/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import *
 
 def index(request):
@@ -7,6 +7,9 @@ def index(request):
     })
 
 def about(request, slug_about):
+    page_data = get_object_or_404(About, slug=slug_about)
+
     return render(request, 'homepage/about.html', {
         'h1': 'Metalworkind',
+        'item': page_data,
     })
