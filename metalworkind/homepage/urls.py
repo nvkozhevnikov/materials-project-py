61207757fcb58683a84adaa082ce9390ee541684
Nick <info@tocomm.ru>
2022-03-30 10:52:21 +0300

    form homepage

--- metalworkind/homepage/urls.py
+++ metalworkind/homepage/urls.py
@@ -4,5 +4,7 @@ from .views import *
 app_name = 'homepage'
 urlpatterns = [
     path('', index, name='homepage'),
+    path('subscribe/', subscribe, name='subscribe'),
+    path('search/', search, name='search'),
     path('about/<slug:slug_about>/', about, name='about'),
 ]
--- metalworkind/homepage/views.py
+++ metalworkind/homepage/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
 from .models import *
+from .forms import AboutForm
 
 def index(request):
     return render(request, 'homepage/index.html', {
@@ -8,8 +10,25 @@ def index(request):
 
 def about(request, slug_about):
     page_data = get_object_or_404(About, slug=slug_about)
+    if request.method == 'POST':
+        form = AboutForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect(request.path)
+    else:
+        form = AboutForm()
 
     return render(request, 'homepage/about.html', {
         'h1': 'Metalworkind',
         'item': page_data,
+        'form': form,
+    })
+
+def subscribe(request):
+    return render(request, 'homepage/index.html', {
+        'h1': 'Metalworkind',
+    })
+
+def search(request):
+    return render(request, 'homepage/index.html', {
+        'h1': 'Metalworkind',
     })
