02ab6e4dd3b75f62f89c94a42723ae2b2ba13be3
Nick <info@tocomm.ru>
2022-04-04 11:27:57 +0300

    gosts metatags

--- metalworkind/gosts/views.py
+++ metalworkind/gosts/views.py
@@ -10,11 +10,6 @@ class Index(ListView):
     def get_queryset(self):
         return GostSections.objects.all()
 
-    # def get_context_data(self, *, object_list=None, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     context['h1'] = 'Справочник'
-    #     return context
-
 class ShowGostSection(ListView):
     model = GostSections
     context_object_name = 'items'
