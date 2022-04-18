0f586b8fb8856714fffd69c7e53df81e7674d1a0
Nick <info@tocomm.ru>
2022-04-18 16:48:43 +0300

    gosts new way url generate in gost index html

--- metalworkind/gosts/views.py
+++ metalworkind/gosts/views.py
@@ -10,6 +10,11 @@ class Index(ListView):
     def get_queryset(self):
         return GostSections.objects.all()
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['sub_sections'] = GostSubSections.objects.all()
+        return context
+
 class ShowGostSection(ListView):
     model = GostSections
     context_object_name = 'items'
