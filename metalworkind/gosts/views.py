63b694b372af23779281426c32ab77f9f2dfb930
Nick <info@tocomm.ru>
2022-04-28 11:39:30 +0300

    bug fix

--- metalworkind/gosts/views.py
+++ metalworkind/gosts/views.py
@@ -14,7 +14,7 @@ class Index(DataMixin, ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['sub_sections'] = GostSubSections.objects.all()
+        context['sub_sections'] = GostSubSections.objects.filter(is_published=True)
         return context
 
 class ShowGostSection(DataMixin, ListView):
--- metalworkind/homepage/urls.py
+++ metalworkind/homepage/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import *
+from django.views.generic.base import TemplateView
 
 app_name = 'homepage'
 urlpatterns = [
@@ -7,6 +8,7 @@ urlpatterns = [
     path('subscribe/', subscribe, name='subscribe'),
     path('search/', Search.as_view(), name='search'),
     path('about/<slug:slug_about>/', about, name='about'),
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain'),),
     # path('test/', test, name='test'),
 ]
 
