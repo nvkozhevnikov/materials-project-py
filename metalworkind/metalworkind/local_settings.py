79494e38cad798cd629efc60975495edd78d1abd
Nick <info@tocomm.ru>
2022-04-28 18:03:25 +0300

    ignore local settings

--- metalworkind/metalworkind/local_settings.py
+++ metalworkind/metalworkind/local_settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on in production! 2
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
