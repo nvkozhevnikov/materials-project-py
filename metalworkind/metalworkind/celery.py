f25be5ca44bf5e01c17b502b70137f3fb645eeb9
Nick <info@tocomm.ru>
2022-04-28 15:05:54 +0300

    bug fix

--- metalworkind/metalworkind/celery.py
+++ metalworkind/metalworkind/celery.py
@@ -7,11 +7,12 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'metalworkind.settings')
 app = Celery('metalworkind')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+app.conf.timezone = 'Europe/Moscow'
 
 app.conf.beat_schedule = {
     'download-currency-everyday': {
         'task': 'homepage.tasks.download_currency_rate',
-        'schedule': crontab(hour='*/24'),
+        'schedule': crontab(minute=0, hour=3),
     },
     'download-metal_prices_every_3h': {
         'task': 'homepage.tasks.download_metal_prices',
